0d6d31bd7ef00382157f2e4a5ee90eadbe51f1c0
AbeAsseged <abenezerdaniel@live.com>
2023-09-08 00:35:18 -0500

    PART One: Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -6,13 +6,23 @@ class Show extends React.Component {
 
     return (
       <div>
-        <h1> Catch Your Pokemon</h1>
+        <h1> Gotta Catch 'Em All</h1>
         <div>
+        <h2>
+        <li key={i}>
+                  The{' '}
+                    <a href={`/fruits/${i}`}>
+                      {pokemon.name}
+                    </a>
+        </li>
+    </h2>
+    
 
+    <a href="/pokemon/">back</a>
 
         </div>
        
-        <img src={img} alt="" />
+        <img src={img} alt=".jpg" />
       </div>
     );
   }
